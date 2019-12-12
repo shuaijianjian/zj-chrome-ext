969e86ebabfeb2760b33040bdfeac1bf38c0da23
zhoujian <jian.zhou@litex.io>
2019-12-12 16:24:56 +0800

    update eslint rules

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -6,15 +6,33 @@
         <el-button type="text">操作按钮</el-button>
       </div>
       <div class="content">
-        <div v-for="item in 4" :key="item" class="text item">{{'列表内容 ' + item }}</div>
+        <div v-for="item in 4" :key="item" class="text item">{{'列表内容 ' + params001 }}</div>
       </div>
+      <el-button type="text" @click="clickUp()">click(+1)</el-button>
+      <br />
+      <el-button type="text" @click="clickDown()">click(-1)</el-button>
     </el-card>
   </div>
 </template>
  
 <script>
 export default {
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      params001: 100
+    };
+  },
+  methods: {
+    clickUp() {
+      ++this.params001;
+      console.log("=============clickUp=======================");
+    },
+    clickDown() {
+      --this.params001;
+      console.log("==============clickDown======================");
+    }
+  }
 };
 </script>
  
